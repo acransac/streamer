af6586bd839e470e4ed75dee47e0bca4bf576078
Adrien Cransac <adrien.cransac@gmail.com>
2019-09-03 22:12:08 +0200

    Added streaming of the continuation which is a later program to execute, built by the current one

--- streamer.js
+++ streamer.js
@@ -9,7 +9,7 @@ Source.from = function (eventEmitter, emissionCallbackName) {
 };
 
 Source.prototype.withDownstream = function (continuation) {
-  this.emitter[this.emissionCallbackName] = (value) => continuation(makeStream(value, new Promise((resolve) => {
+  this.emitter[this.emissionCallbackName] = (value) => continuation(makeStream([value, null], new Promise((resolve) => {
     this.emitter[this.emissionCallbackName] = (value) => resolve(value);
   }), this));
 
@@ -32,18 +32,35 @@ function source(stream) {
   return stream[2];
 }
 
+function value(now) {
+  return now[0];
+}
+
+function continuation(now) {
+  return now[1];
+}
+
 async function later(stream) {
-  return makeStream(await afterwards(stream), new Promise((resolve) => {
+  return makeStream([await afterwards(stream), continuation(now(stream))], new Promise((resolve) => {
     source(stream).emitter[source(stream).emissionCallbackName] = (value) => resolve(value);
   }), source(stream));
 }
 
 function floatOn(stream, jsValue) {
-  return makeStream(jsValue, afterwards(stream), source(stream));
+  return makeStream([jsValue, continuation(now(stream))], afterwards(stream), source(stream));
+}
+
+function commit(stream, contract) {
+  if (continuation(now(stream)) === null) {
+    return makeStream([value(now(stream)), contract], afterwards(stream), source(stream));
+  }
+  else {
+    return makeStream([value(now(stream)), async (futureStream) => contract(await continuation(now(stream))(futureStream))], afterwards(stream), source(stream));
+  }
 }
 
 function IO(procedure, ioChannel) {
   return (stream) => procedure(ioChannel)(stream);
 }
 
-module.exports = { Source, now, later, floatOn, IO };
+module.exports = { Source, now, later, value, continuation, floatOn, commit, IO };
